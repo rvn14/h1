04c44cee9cdf54d16a190436873d4e60db08a939
Dasun Adithya <122367717+rvn14@users.noreply.github.com>
2025-01-03 10:21:41 +0530

    Delete db linked directory

--- db linked/ddr.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbServerName = "localhost";
-$dbUserName = "root";
-$dbPassword = "";
-$dbName = "first_database";
-
-$connect = mysqli_connect($dbServerName , $dbUserName , $dbPassword , $dbName);
-?>
